80b05ac8ea6040c3d8a8e0759cf3d37d2b7ab08f
Paweł Byszewski <pawel.byszewski@polidea.com>
2017-06-29 14:06:17 +0200

    android library support

--- src/main/groovy/com/tmiyamon/config/ConfigPlugin.groovy
+++ src/main/groovy/com/tmiyamon/config/ConfigPlugin.groovy
@@ -1,30 +1,57 @@
 package com.tmiyamon.config
 
+import com.android.build.gradle.LibraryExtension
 import com.tmiyamon.config.task.GenerateSettingsTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import com.android.build.gradle.api.BaseVariant
 import com.android.build.gradle.AppExtension
+import org.gradle.api.Task
 
 class ConfigPlugin implements Plugin<Project> {
+
+    enum ProjectType {
+
+        ANDROID_APPLICATION('com.android.application', AppExtension),
+        ANDROID_LIBRARY('com.android.library', LibraryExtension)
+
+        final String pluginId
+        final Class extensionType
+
+        ProjectType(String pluginId, Class extensionType) {
+            this.pluginId = pluginId
+            this.extensionType = extensionType
+        }
+    }
+
     @Override
     void apply(Project project) {
-        project.plugins.withId('com.android.application') {
-            def android = project.extensions.getByType(AppExtension)
-            android.applicationVariants.all { BaseVariant variant ->
+        ProjectType.values().each {
+            createCodeGenerationTasks(project, it)
+        }
+    }
 
-                def task = project.tasks.create(
-                    name: "generate${variant.name.capitalize()}Settings",
-                    type: GenerateSettingsTask) {
-                        packageName    variant.generateBuildConfig.buildConfigPackageName
-                        flavorName     variant.flavorName
-                        buildTypeName  variant.buildType.name
-                        variantDirName variant.dirName
-                    }
+    private createCodeGenerationTasks(Project project, ProjectType projectType) {
+        project.plugins.withId(projectType.pluginId) {
+            def android = project.extensions.getByType(projectType.extensionType)
+            android.libraryVariants.all { BaseVariant variant ->
+
+                def task = createCodeGenerationTask(project, variant)
 
                 variant.registerJavaGeneratingTask(task, task.outputDir())
                 android.sourceSets[variant.name].java.srcDirs += [task.outputDir()]
             }
+        }
+    }
+
+    private Task createCodeGenerationTask(Project project, BaseVariant variant) {
+        project.tasks.create(
+                name: "generate${variant.name.capitalize()}Settings",
+                type: GenerateSettingsTask) {
+            packageName variant.generateBuildConfig.buildConfigPackageName
+            flavorName variant.flavorName
+            buildTypeName variant.buildType.name
+            variantDirName variant.dirName
         }
     }
 }
