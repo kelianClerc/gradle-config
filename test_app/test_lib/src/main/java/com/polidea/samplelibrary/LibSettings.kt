513803b3992e0a46edb334ee0f1647d741236808
s.jcqs <satyan.jacquens@fabernovel.com>
2020-06-02 11:24:04 +0200

    If it doesn't exist flavor is not generated

--- test_app/test_lib/src/main/java/com/polidea/samplelibrary/LibSettings.kt
+++ test_app/test_lib/src/main/java/com/polidea/samplelibrary/LibSettings.kt
@@ -3,7 +3,7 @@ package com.polidea.samplelibrary
 class LibSettings {
 
     val buildVariant: String
-        get() = "${BuildConfig.FLAVOR}_${BuildConfig.BUILD_TYPE}"
+        get() = BuildConfig.BUILD_TYPE
 
     val propertyValue: String
         get() = Settings.lib_properties.lib_property_1
